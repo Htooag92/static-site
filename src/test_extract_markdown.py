d2bdbbec04a0b9c4bbf7fa96f7bb2ef27f7a655f
Htoo Aung <htooaung.ezio@gmail.com>
2025-04-08 11:55:45 +0700

    Add test for markdown_to_blocks function

--- src/test_extract_markdown.py
+++ src/test_extract_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_extract import extract_markdown_images
+from markdown_extract import extract_markdown_images, markdown_to_blocks
 
 class TestMarkdownExtraction(unittest.TestCase):
     def test_extract_markdown_images(self):
@@ -13,6 +13,23 @@ class TestMarkdownExtraction(unittest.TestCase):
         matches = extract_markdown_images(text)
         expected = [("image1", "https://example.com/img1.png"), ("image2", "https://example.com/img2.png")]
         self.assertListEqual(matches, expected)
+        
+    def test_markdown_to_blocks(self):
+        md = """
+This is **bolded** paragraph
 
-if __name__ == "__main__":
-    unittest.main()
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
